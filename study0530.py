ce444f6ee156146fff0fce1bebf7001191677200
QuinlanVictor <qianliang1994@126.com>
2020-06-07 18:49:45 +0800

    学习代码0530
    学习代码0530

--- study0530.py
+++ study0530.py
@@ -1,239 +1,246 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Mar 21 17:25:30 2018
-
-博客地址 ：http://blog.csdn.net/u013733326/article/details/79639509
-
-@author: Oscar
-"""
-
+#Course1 神经网络和深度学习 第三周作业  带有一个隐藏层的平面数据分类
 import numpy as np
 import matplotlib.pyplot as plt
-import h5py
-from lr_utils import load_dataset
+from testCases import *
+import sklearn
+import sklearn.datasets
+import sklearn.linear_model
+from planar_utils import plot_decision_boundary, sigmoid, load_planar_dataset, load_extra_datasets
 
-train_set_x_orig , train_set_y , test_set_x_orig , test_set_y , classes = load_dataset()
+np.random.seed(1)
 
-m_train = train_set_y.shape[1] #训练集里图片的数量。
-m_test = test_set_y.shape[1] #测试集里图片的数量。
-num_px = train_set_x_orig.shape[1] #训练、测试集里面的图片的宽度和高度（均为64x64）。
+X, Y = load_planar_dataset()
 
-#现在看一看我们加载的东西的具体情况
-print ("训练集的数量: m_train = " + str(m_train))
-print ("测试集的数量 : m_test = " + str(m_test))
-print ("每张图片的宽/高 : num_px = " + str(num_px))
-print ("每张图片的大小 : (" + str(num_px) + ", " + str(num_px) + ", 3)")
-print ("训练集_图片的维数 : " + str(train_set_x_orig.shape))
-print ("训练集_标签的维数 : " + str(train_set_y.shape))
-print ("测试集_图片的维数: " + str(test_set_x_orig.shape))
-print ("测试集_标签的维数: " + str(test_set_y.shape))
+plt.scatter(X[0, :], X[1, :], c=Y, s=40, cmap=plt.cm.Spectral)
 
-#将训练集的维度降低并转置。
-train_set_x_flatten  = train_set_x_orig.reshape(train_set_x_orig.shape[0],-1).T
-#将测试集的维度降低并转置。
-test_set_x_flatten = test_set_x_orig.reshape(test_set_x_orig.shape[0], -1).T
+clf = sklearn.linear_model.LogisticRegressionCV()
+clf.fit(X.T,Y.T)
 
-print ("训练集降维最后的维度： " + str(train_set_x_flatten.shape))
-print ("训练集_标签的维数 : " + str(train_set_y.shape))
-print ("测试集降维之后的维度: " + str(test_set_x_flatten.shape))
-print ("测试集_标签的维数 : " + str(test_set_y.shape))
 
-train_set_x = train_set_x_flatten / 255
-test_set_x = test_set_x_flatten / 255
-
-def sigmoid(z):
+def layer_sizes(X , Y):
     """
     参数：
-        z  - 任何大小的标量或numpy数组。
+     X - 输入数据集,维度为（输入的数量，训练/测试的数量）
+     Y - 标签，维度为（输出的数量，训练/测试数量）
 
     返回：
-        s  -  sigmoid（z）
+     n_x - 输入层的数量
+     n_h - 隐藏层的数量
+     n_y - 输出层的数量
     """
-    s = 1 / (1 + np.exp(-z))
-    return s
+    n_x = X.shape[0] #输入层
+    n_h = 4 #，隐藏层，硬编码为4
+    n_y = Y.shape[0] #输出层
 
-def initialize_with_zeros(dim):
+    return (n_x,n_h,n_y)
+
+
+
+def initialize_parameters( n_x , n_h ,n_y):
     """
-        此函数为w创建一个维度为（dim，1）的0向量，并将b初始化为0。
-
-        参数：
-            dim  - 我们想要的w矢量的大小（或者这种情况下的参数数量）
-
-        返回：
-            w  - 维度为（dim，1）的初始化向量。
-            b  - 初始化的标量（对应于偏差）
-    """
-    w = np.zeros(shape = (dim,1))
-    b = 0
-    #使用断言来确保我要的数据是正确的
-    assert(w.shape == (dim, 1)) #w的维度是(dim,1)
-    assert(isinstance(b, float) or isinstance(b, int)) #b的类型是float或者是int
-
-    return (w , b)
-
-def propagate(w, b, X, Y):
-    """
-    实现前向和后向传播的成本函数及其梯度。
     参数：
-        w  - 权重，大小不等的数组（num_px * num_px * 3，1）
-        b  - 偏差，一个标量
-        X  - 矩阵类型为（num_px * num_px * 3，训练数量）
-        Y  - 真正的“标签”矢量（如果非猫则为0，如果是猫则为1），矩阵维度为(1,训练数据数量)
+        n_x - 输入层节点的数量
+        n_h - 隐藏层节点的数量
+        n_y - 输出层节点的数量
 
     返回：
-        cost- 逻辑回归的负对数似然成本
-        dw  - 相对于w的损失梯度，因此与w相同的形状
-        db  - 相对于b的损失梯度，因此与b的形状相同
+        parameters - 包含参数的字典：
+            W1 - 权重矩阵,维度为（n_h，n_x）
+            b1 - 偏向量，维度为（n_h，1）
+            W2 - 权重矩阵，维度为（n_y，n_h）
+            b2 - 偏向量，维度为（n_y，1）
+
+    """
+    np.random.seed(2) #指定一个随机种子，以便你的输出与我们的一样。
+    W1 = np.random.randn(n_h,n_x) * 0.01
+    b1 = np.zeros(shape=(n_h, 1))
+    W2 = np.random.randn(n_y,n_h) * 0.01
+    b2 = np.zeros(shape=(n_y, 1))
+
+    #使用断言确保我的数据格式是正确的
+    assert(W1.shape == ( n_h , n_x ))
+    assert(b1.shape == ( n_h , 1 ))
+    assert(W2.shape == ( n_y , n_h ))
+    assert(b2.shape == ( n_y , 1 ))
+
+    parameters = {"W1" : W1,
+	              "b1" : b1,
+	              "W2" : W2,
+	              "b2" : b2 }
+
+    return parameters
+
+
+def forward_propagation( X , parameters ):
+    """
+    参数：
+         X - 维度为（n_x，m）的输入数据。
+         parameters - 初始化函数（initialize_parameters）的输出
+
+    返回：
+         A2 - 使用sigmoid()函数计算的第二次激活后的数值
+         cache - 包含“Z1”，“A1”，“Z2”和“A2”的字典类型变量
+     """
+    W1 = parameters["W1"]
+    b1 = parameters["b1"]
+    W2 = parameters["W2"]
+    b2 = parameters["b2"]
+    #前向传播计算A2
+    Z1 = np.dot(W1 , X) + b1
+    A1 = np.tanh(Z1)
+    Z2 = np.dot(W2 , A1) + b2
+    A2 = sigmoid(Z2)
+    #使用断言确保我的数据格式是正确的
+    assert(A2.shape == (1,X.shape[1]))
+    cache = {"Z1": Z1,
+             "A1": A1,
+             "Z2": Z2,
+             "A2": A2}
+
+    return (A2, cache)
+
+
+def compute_cost(A2,Y,parameters):
+    """
+    计算方程（6）中给出的交叉熵成本，
+
+    参数：
+         A2 - 使用sigmoid()函数计算的第二次激活后的数值
+         Y - "True"标签向量,维度为（1，数量）
+         parameters - 一个包含W1，B1，W2和B2的字典类型的变量
+
+    返回：
+         成本 - 交叉熵成本给出方程（13）
+    """
+
+    m = Y.shape[1]
+    W1 = parameters["W1"]
+    W2 = parameters["W2"]
+
+    #计算成本
+    logprobs = logprobs = np.multiply(np.log(A2), Y) + np.multiply((1 - Y), np.log(1 - A2))
+    cost = - np.sum(logprobs) / m
+    cost = float(np.squeeze(cost))
+
+    assert(isinstance(cost,float))
+
+    return cost
+
+
+def backward_propagation(parameters,cache,X,Y):
+    """
+    使用上述说明搭建反向传播函数。
+
+    参数：
+     parameters - 包含我们的参数的一个字典类型的变量。
+     cache - 包含“Z1”，“A1”，“Z2”和“A2”的字典类型的变量。
+     X - 输入数据，维度为（2，数量）
+     Y - “True”标签，维度为（1，数量）
+
+    返回：
+     grads - 包含W和b的导数一个字典类型的变量。
     """
     m = X.shape[1]
 
-    #正向传播
-    A = sigmoid(np.dot(w.T,X) + b) #计算激活值，请参考公式2。
-    cost = (- 1 / m) * np.sum(Y * np.log(A) + (1 - Y) * (np.log(1 - A))) #计算成本，请参考公式3和4。
+    W1 = parameters["W1"]
+    W2 = parameters["W2"]
 
-    #反向传播
-    dw = (1 / m) * np.dot(X, (A - Y).T) #请参考视频中的偏导公式。
-    db = (1 / m) * np.sum(A - Y) #请参考视频中的偏导公式。
+    A1 = cache["A1"]
+    A2 = cache["A2"]
 
-    #使用断言确保我的数据是正确的
-    assert(dw.shape == w.shape)
-    assert(db.dtype == float)
-    cost = np.squeeze(cost)
-    assert(cost.shape == ())
+    dZ2= A2 - Y
+    dW2 = (1 / m) * np.dot(dZ2, A1.T)
+    db2 = (1 / m) * np.sum(dZ2, axis=1, keepdims=True)
+    dZ1 = np.multiply(np.dot(W2.T, dZ2), 1 - np.power(A1, 2))
+    dW1 = (1 / m) * np.dot(dZ1, X.T)
+    db1 = (1 / m) * np.sum(dZ1, axis=1, keepdims=True)
+    grads = {"dW1": dW1,
+             "db1": db1,
+             "dW2": dW2,
+             "db2": db2 }
 
-    #创建一个字典，把dw和db保存起来。
-    grads = {
-                "dw": dw,
-                "db": db
-             }
-    return (grads , cost)
+    return grads
 
-def optimize(w , b , X , Y , num_iterations , learning_rate , print_cost = False):
+
+def update_parameters(parameters,grads,learning_rate=1.2):
     """
-    此函数通过运行梯度下降算法来优化w和b
+    使用上面给出的梯度下降更新规则更新参数
 
     参数：
-        w  - 权重，大小不等的数组（num_px * num_px * 3，1）
-        b  - 偏差，一个标量
-        X  - 维度为（num_px * num_px * 3，训练数据的数量）的数组。
-        Y  - 真正的“标签”矢量（如果非猫则为0，如果是猫则为1），矩阵维度为(1,训练数据的数量)
-        num_iterations  - 优化循环的迭代次数
-        learning_rate  - 梯度下降更新规则的学习率
-        print_cost  - 每100步打印一次损失值
+     parameters - 包含参数的字典类型的变量。
+     grads - 包含导数值的字典类型的变量。
+     learning_rate - 学习速率
 
     返回：
-        params  - 包含权重w和偏差b的字典
-        grads  - 包含权重和偏差相对于成本函数的梯度的字典
-        成本 - 优化期间计算的所有成本列表，将用于绘制学习曲线。
-
-    提示：
-    我们需要写下两个步骤并遍历它们：
-        1）计算当前参数的成本和梯度，使用propagate（）。
-        2）使用w和b的梯度下降法则更新参数。
+     parameters - 包含更新参数的字典类型的变量。
     """
+    W1,W2 = parameters["W1"],parameters["W2"]
+    b1,b2 = parameters["b1"],parameters["b2"]
 
-    costs = []
+    dW1,dW2 = grads["dW1"],grads["dW2"]
+    db1,db2 = grads["db1"],grads["db2"]
+
+    W1 = W1 - learning_rate * dW1
+    b1 = b1 - learning_rate * db1
+    W2 = W2 - learning_rate * dW2
+    b2 = b2 - learning_rate * db2
+
+    parameters = {"W1": W1,
+                  "b1": b1,
+                  "W2": W2,
+                  "b2": b2}
+
+    return parameters
+
+def nn_model(X,Y,n_h,num_iterations,print_cost=False):
+    """
+    参数：
+        X - 数据集,维度为（2，示例数）
+        Y - 标签，维度为（1，示例数）
+        n_h - 隐藏层的数量
+        num_iterations - 梯度下降循环中的迭代次数
+        print_cost - 如果为True，则每1000次迭代打印一次成本数值
+
+    返回：
+        parameters - 模型学习的参数，它们可以用来进行预测。
+     """
+
+    np.random.seed(3) #指定随机种子
+    n_x = layer_sizes(X, Y)[0]
+    n_y = layer_sizes(X, Y)[2]
+
+    parameters = initialize_parameters(n_x,n_h,n_y)
+    W1 = parameters["W1"]
+    b1 = parameters["b1"]
+    W2 = parameters["W2"]
+    b2 = parameters["b2"]
 
     for i in range(num_iterations):
+        A2 , cache = forward_propagation(X,parameters)
+        cost = compute_cost(A2,Y,parameters)
+        grads = backward_propagation(parameters,cache,X,Y)
+        parameters = update_parameters(parameters,grads,learning_rate = 0.5)
 
-        grads, cost = propagate(w, b, X, Y)
+        if print_cost:
+            if i%1000 == 0:
+                print("第 ",i," 次循环，成本为："+str(cost))
+    return parameters
 
-        dw = grads["dw"]
-        db = grads["db"]
 
-        w = w - learning_rate * dw
-        b = b - learning_rate * db
-
-        #记录成本
-        if i % 100 == 0:
-            costs.append(cost)
-        #打印成本数据
-        if (print_cost) and (i % 100 == 0):
-            print("迭代的次数: %i ， 误差值： %f" % (i,cost))
-
-    params  = {
-                "w" : w,
-                "b" : b }
-    grads = {
-            "dw": dw,
-            "db": db }
-    return (params , grads , costs)
-
-def predict(w , b , X ):
+def predict(parameters,X):
     """
-    使用学习逻辑回归参数logistic （w，b）预测标签是0还是1，
-
+    使用学习的参数，为X中的每个示例预测一个类
+    
     参数：
-        w  - 权重，大小不等的数组（num_px * num_px * 3，1）
-        b  - 偏差，一个标量
-        X  - 维度为（num_px * num_px * 3，训练数据的数量）的数据
+		parameters - 包含参数的字典类型的变量。
+	    X - 输入数据（n_x，m）
+    
+    返回
+		predictions - 我们模型预测的向量（红色：0 /蓝色：1）
+     
+     """
+    A2 , cache = forward_propagation(X,parameters)
+    predictions = np.round(A2)
+    
+    return predictions
 
-    返回：
-        Y_prediction  - 包含X中所有图片的所有预测【0 | 1】的一个numpy数组（向量）
-
-    """
-
-    m  = X.shape[1] #图片的数量
-    Y_prediction = np.zeros((1,m))
-    w = w.reshape(X.shape[0],1)
-
-    #计预测猫在图片中出现的概率
-    A = sigmoid(np.dot(w.T , X) + b)
-    for i in range(A.shape[1]):
-        #将概率a [0，i]转换为实际预测p [0，i]
-        Y_prediction[0,i] = 1 if A[0,i] > 0.5 else 0
-    #使用断言
-    assert(Y_prediction.shape == (1,m))
-
-    return Y_prediction
-
-def model(X_train , Y_train , X_test , Y_test , num_iterations = 2000 , learning_rate = 0.5 , print_cost = False):
-    """
-    通过调用之前实现的函数来构建逻辑回归模型
-
-    参数：
-        X_train  - numpy的数组,维度为（num_px * num_px * 3，m_train）的训练集
-        Y_train  - numpy的数组,维度为（1，m_train）（矢量）的训练标签集
-        X_test   - numpy的数组,维度为（num_px * num_px * 3，m_test）的测试集
-        Y_test   - numpy的数组,维度为（1，m_test）的（向量）的测试标签集
-        num_iterations  - 表示用于优化参数的迭代次数的超参数
-        learning_rate  - 表示optimize（）更新规则中使用的学习速率的超参数
-        print_cost  - 设置为true以每100次迭代打印成本
-
-    返回：
-        d  - 包含有关模型信息的字典。
-    """
-    w , b = initialize_with_zeros(X_train.shape[0])
-
-    parameters , grads , costs = optimize(w , b , X_train , Y_train,num_iterations , learning_rate , print_cost)
-
-    #从字典“参数”中检索参数w和b
-    w , b = parameters["w"] , parameters["b"]
-
-    #预测测试/训练集的例子
-    Y_prediction_test = predict(w , b, X_test)
-    Y_prediction_train = predict(w , b, X_train)
-
-    #打印训练后的准确性
-    print("训练集准确性："  , format(100 - np.mean(np.abs(Y_prediction_train - Y_train)) * 100) ,"%")
-    print("测试集准确性："  , format(100 - np.mean(np.abs(Y_prediction_test - Y_test)) * 100) ,"%")
-
-    d = {
-            "costs" : costs,
-            "Y_prediction_test" : Y_prediction_test,
-            "Y_prediciton_train" : Y_prediction_train,
-            "w" : w,
-            "b" : b,
-            "learning_rate" : learning_rate,
-            "num_iterations" : num_iterations }
-    return d
-
-d = model(train_set_x, train_set_y, test_set_x, test_set_y, num_iterations = 2000, learning_rate = 0.005, print_cost = True)
-
-#绘制图
-costs = np.squeeze(d['costs'])
-plt.plot(costs)
-plt.ylabel('cost')
-plt.xlabel('iterations (per hundreds)')
-plt.title("Learning rate =" + str(d["learning_rate"]))
-plt.show()
