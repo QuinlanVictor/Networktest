a7948ae562275b01db963321a1a2b8466c5b1f3e
QuinlanVictor <qianliang1994@126.com>
2020-06-10 23:13:26 +0800

    学习代码0603
    tensorflow学习

--- study0603.py
+++ study0603.py
@@ -1,3 +1,40 @@
+def initialize_parameters():
+    """
+    初始化神经网络的参数，参数的维度如下：
+        W1 : [25, 12288]
+        b1 : [25, 1]
+        W2 : [12, 25]
+        b2 : [12, 1]
+        W3 : [6, 12]
+        b3 : [6, 1]
+    
+    返回：
+        parameters - 包含了W和b的字典
+    
+    
+    """
+    
+    tf.set_random_seed(1) #指定随机种子
+    
+    W1 = tf.get_variable("W1",[25,12288],initializer=tf.contrib.layers.xavier_initializer(seed=1))
+    b1 = tf.get_variable("b1",[25,1],initializer=tf.zeros_initializer())
+    W2 = tf.get_variable("W2", [12, 25], initializer = tf.contrib.layers.xavier_initializer(seed=1))
+    b2 = tf.get_variable("b2", [12, 1], initializer = tf.zeros_initializer())
+    W3 = tf.get_variable("W3", [6, 12], initializer = tf.contrib.layers.xavier_initializer(seed=1))
+    b3 = tf.get_variable("b3", [6, 1], initializer = tf.zeros_initializer())
+    
+    parameters = {"W1": W1,
+                  "b1": b1,
+                  "W2": W2,
+                  "b2": b2,
+                  "W3": W3,
+                  "b3": b3}
+    
+    return parameters
+
+
+
+
 def model(X_train,Y_train,X_test,Y_test,
 		learning_rate=0.0001,num_epochs=1500,minibatch_size=32,
 		print_cost=True,is_plot=True):
