8f6b2daf1db39bdc9c8b55c26c25171c4ec9daff
QuinlanVictor <qianliang1994@126.com>
2020-06-13 23:20:29 +0800

    学习代码0606
    车辆识别

--- study0606.py
+++ study0606.py
@@ -1,1 +1,36 @@
 #Course4 卷积神经网络 第三周作业  车辆识别
+def yolo_filter_boxes(box_confidence , boxes, box_class_probs, threshold = 0.6):
+    """
+    通过阈值来过滤对象和分类的置信度。
+    
+    参数：
+        box_confidence  - tensor类型，维度为（19,19,5,1）,包含19x19单元格中每个单元格预测的5个锚框中的所有的锚框的pc （一些对象的置信概率）。
+        boxes - tensor类型，维度为(19,19,5,4)，包含了所有的锚框的（px,py,ph,pw ）。
+        box_class_probs - tensor类型，维度为(19,19,5,80)，包含了所有单元格中所有锚框的所有对象( c1,c2,c3，···，c80 )检测的概率。
+        threshold - 实数，阈值，如果分类预测的概率高于它，那么这个分类预测的概率就会被保留。
+    
+    返回：
+        scores - tensor 类型，维度为(None,)，包含了保留了的锚框的分类概率。
+        boxes - tensor 类型，维度为(None,4)，包含了保留了的锚框的(b_x, b_y, b_h, b_w)
+        classess - tensor 类型，维度为(None,)，包含了保留了的锚框的索引
+        
+    注意："None"是因为你不知道所选框的确切数量，因为它取决于阈值。
+          比如：如果有10个锚框，scores的实际输出大小将是（10,）
+    """
+    
+    #第一步：计算锚框的得分
+    box_scores  = box_confidence * box_class_probs
+    
+    #第二步：找到最大值的锚框的索引以及对应的最大值的锚框的分数
+    box_classes = K.argmax(box_scores, axis=-1)
+    box_class_scores = K.max(box_scores, axis=-1)
+    
+    #第三步：根据阈值创建掩码
+    filtering_mask = (box_class_scores >= threshold)
+    
+    #对scores, boxes 以及 classes使用掩码
+    scores = tf.boolean_mask(box_class_scores,filtering_mask)
+    boxes = tf.boolean_mask(boxes,filtering_mask)
+    classes = tf.boolean_mask(box_classes,filtering_mask)
+    
+    return scores , boxes , classes
