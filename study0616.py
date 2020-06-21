045649052148db763bedfcb71964499be995a5d3
QuinlanVictor <qianliang1994@126.com>
2020-06-21 23:17:12 +0800

    学习代码0616
    人脸识别与神经风格转换

--- study0616.py
+++ study0616.py
@@ -66,3 +66,36 @@ def triplet_loss(y_true, y_pred, alpha = 0.2):
     loss = tf.reduce_sum(tf.maximum(basic_loss,0))
     
     return loss
+
+
+def triplet_loss(y_true, y_pred, alpha = 0.2):
+    """
+    根据公式（4）实现三元组损失函数
+    
+    参数：
+        y_true -- true标签，当你在Keras里定义了一个损失函数的时候需要它，但是这里不需要。
+        y_pred -- 列表类型，包含了如下参数：
+            anchor -- 给定的“anchor”图像的编码，维度为(None,128)
+            positive -- “positive”图像的编码，维度为(None,128)
+            negative -- “negative”图像的编码，维度为(None,128)
+        alpha -- 超参数，阈值
+    
+    返回：
+        loss -- 实数，损失的值
+    """
+    #获取anchor, positive, negative的图像编码
+    anchor, positive, negative = y_pred[0], y_pred[1], y_pred[2]
+    
+    #第一步：计算"anchor" 与 "positive"之间编码的距离，这里需要使用axis=-1
+    pos_dist = tf.reduce_sum(tf.square(tf.subtract(anchor,positive)),axis=-1)
+    
+    #第二步：计算"anchor" 与 "negative"之间编码的距离，这里需要使用axis=-1
+    neg_dist = tf.reduce_sum(tf.square(tf.subtract(anchor,negative)),axis=-1)
+    
+    #第三步：减去之前的两个距离，然后加上alpha
+    basic_loss = tf.add(tf.subtract(pos_dist,neg_dist),alpha)
+    
+    #通过取带零的最大值和对训练样本的求和来计算整个公式
+    loss = tf.reduce_sum(tf.maximum(basic_loss,0))
+    
+    return loss
